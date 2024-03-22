065ce3567f6e8daa381317601bbecc5afa93f5f0
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-22 19:54:30 -0300

    :sparkles: feature: added the functions that generates the html output file/string

--- data_to_html.py
+++ data_to_html.py
@@ -1,4 +1,12 @@
 import random as r
+import jinja2 as j2
+
+TEMPLATE_HTML_PADRAO = "template/base.html"
+IMAGEM_PADRAO = "https://i.pinimg.com/originals/7a/7e/a1/7a7ea1d7fbededa69475b\
+                26829077e0c.png".replace(" ", "")
+DESCRICAO_PADRAO = "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+OUTPUT_PADRAO = "index.output.html"
+FONT_TAMANHO_PADRAO = "9.125pt"
 
 
 def converter_para_matriz(lista_num, colunas_pp=2, fichas_pc=3, celulas_pf=4):
@@ -59,7 +67,82 @@ def converter_para_matriz(lista_num, colunas_pp=2, fichas_pc=3, celulas_pf=4):
     return resultado
 
 
+def gerar_html(matriz, template_html=TEMPLATE_HTML_PADRAO,
+               imagem=IMAGEM_PADRAO, descricao=DESCRICAO_PADRAO,
+               font_tamanho=FONT_TAMANHO_PADRAO):
+    f"""Dado um template, essa função vai gerar o HTML baseado nos dados da
+    matriz de fichas nos parâmetros. As dimensões de cada página será definida
+    pelo formato da matríz, por exemplo, quantas sublistas ela vai ter vai
+    determinar a quantidade de páginas no total, as sub sub listas a quantidade
+    de colunas, e assim por diante.
+
+    :param list[list[list[tuple[int]]]] matriz:
+        Uma matríz multidimensional; Lista de páginas, que são listas de
+        fichas, que são lista de células que são tuplas de números.
+    :param str template_html:
+        Caminho para o arquivo de template HTML, precisa ser um arquivo
+        compatível com o Jinja2. O padrão é {TEMPLATE_HTML_PADRAO}.
+    :param str | None imagem:
+        Imagem para o banner de cada ficha, se for uma string vazia ou nulo,
+        a página vai só renderizar um espaço em branco onde o a imagem ficaria.
+        O padrão é {IMAGEM_PADRAO}.
+    :param str descricao:
+        Pequeno texto que vai ficar em baixo de cada ficha, por padrão é apenas
+        um Lorem Ipsum para ocupar espaço.
+    :param str font_tamanho:
+        Tamanho de fonte que o CSS irá utilizar para determinar o tamanho de
+        tudo, não só as fontes. É importante que essa string seja compatível
+        com as unidades de medida do CSS. Por padrão é {FONT_TAMANHO_PADRAO}.
+
+    Um adendo, caso você esteja usando dimensões customizadas para o formato
+    da variável matriz, então você talvez queira mudar o tamanho da fonte,
+    quantidade de colunas, etc. pro conteúdo caber nas páginas.
+
+    :returns str:
+        HTML (não minificado) gerado apartir dos dados providos.
+    """
+    with open(template_html, "r") as th:
+        jinja = j2.Template(th.read())
+
+        return jinja.render({
+            "font_tamanho": font_tamanho,
+            "matriz": matriz,
+            "imagem": imagem or None,
+            "descricao": descricao
+        })
+
+
+def gerar_html_e_escrever(matriz, output, template_html=TEMPLATE_HTML_PADRAO,
+                          imagem=IMAGEM_PADRAO, descricao=DESCRICAO_PADRAO):
+    f"""Essa função funciona em volta da função 'gerar_html()', a diferença é
+    que essa usa o output da anterior pra escrever o HTML gerado num arquivo
+    especificado pelo usuário.
+
+    :param list[list[list[tuple[int]]]] matriz:
+        Uma matríz multidimensional; Lista de páginas, que são listas de
+        fichas, que são lista de células que são tuplas de números.
+    :param str template_html:
+        Caminho para o arquivo de template HTML, precisa ser um arquivo
+        compatível com o Jinja2. O padrão é {TEMPLATE_HTML_PADRAO}.
+    :param str | None imagem:
+        Imagem para o banner de cada ficha, se for uma string vazia ou nulo,
+        a página vai só renderizar um espaço em branco onde o a imagem ficaria.
+        O padrão é {IMAGEM_PADRAO}.
+    :param str descricao:
+        Pequeno texto que vai ficar em baixo de cada ficha, por padrão é apenas
+        um Lorem Ipsum para ocupar espaço.
+    :param str font_tamanho:
+        Tamanho de fonte que o CSS irá utilizar para determinar o tamanho de
+        tudo, não só as fontes. É importante que essa string seja compatível
+        com as unidades de medida do CSS. Por padrão é {FONT_TAMANHO_PADRAO}.
+    """
+    resultado = gerar_html(matriz, template_html, imagem, descricao)
+
+    with open(output, "w") as op:
+        op.write(resultado)
+
+
 if __name__ == "__main__":
     matriz = converter_para_matriz([r.randint(1000, 9999)
-                                    for _ in range(15 * 4)])
-    print(matriz)
+                                    for _ in range(5000 * 4)])
+    gerar_html_e_escrever(matriz, OUTPUT_PADRAO)
